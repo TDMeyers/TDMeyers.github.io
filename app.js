e1b61b67ba96166be1c8de1f158087174f8dc0d3
TDMeyers <meyers.dylan@outlook.com>
2023-06-27 16:15:43 -0600

    added ghost enemy, tiled background to container.

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
+// Create global variables for drawing the game window
+const
+divHeight = document.getElementById('game-portal').clientHeight;
+divWidth = document.getElementById('game-portal').clientWidth;
+
+// Create global variables for drawing maps onto canvas
+const 
+canvas = document.getElementById("game-view");
+ctx = canvas.getContext("2d");
+let offsetX = canvas.offsetLeft;
+let offsetY = canvas.offsetTop;
+
+// Create global variables for button functions
+const
+// - Directionals
+left = document.getElementById("left");
+right = document.getElementById("right");
+up = document.getElementById("up");
+down = document.getElementById("down");
+// - Flashlight
+turnOn = document.getElementById("on");
+turnOff = document.getElementById("off");
+// - Game Controls
+start = document.getElementById("start");
+reset = document.getElementById("reset");
+reset.addEventListener('click', function(event){
+    location.reload();
+})
+// Creating function to draw Canvas element onto page. 
+function createCanvas() {
+    canvas.height = divHeight;
+    canvas.width = divWidth;
+    }
 // Drawing Canvas area to place game window in on document load.
 window.addEventListener('load', function(event){
-    function createCanvas() {
-        let canvas = document.getElementById('game-view');
-        let divHeight = document.getElementById('game-portal').clientHeight;
-        let divWidth = document.getElementById('game-portal').clientWidth;
-        canvas.height = divHeight;
-        canvas.width = divWidth;
-        }
     createCanvas();
     }
 )
@@ -14,10 +40,6 @@ window.addEventListener('load', function(event){
 window.addEventListener('resize', function(event){
     createCanvas();
     }, true )
-
-// Create global variables for drawing maps onto canvas
-let canvas = document.getElementById("game-view");
-let ctx = canvas.getContext("2d");
 
 // Create class for Player character
 class Player {
@@ -27,13 +49,83 @@ class Player {
         this.health = health || 10,
         this.mvmnt = mvmnt || 1
     }
-}
-// Create class for Map Objects
-// Create class for flashlight? (maybe)
 
+}
+// class for randomly moving Spooky object
+class Spooky{
+    constructor(img, x, y, dx, dy, radius){
+        this.Sprite = new Image();
+        this.Sprite.src = img;
+        this.X = x;
+        this.Y = y;
+        this.dx = dx || 1;
+        this.dy = dy || 1;
+        this.radius = radius || 80;
+    }
+
+    moveBounce() {
+        // Movement function for Spooky
+        this.X += this.dx;
+        this.Y += this.dy;
+        
+        // Creating collision function for Spooky 
+        if (this.X + this.dx > divWidth-this.radius || this.X + this.dx < 0) {
+            this.dx = -this.dx;
+        }
+        
+        if (this.Y + this.dy > divHeight-this.radius || this.Y + this.dy < 0) {
+            this.dy = -this.dy;
+        }
+    }
+}
+
+// Create class for Map Objects
+// Create class for flashlight? (maybe - probably not, leaving for stretch goal) 
+// Create variables and DOM display for character stats
 // Create function for collisions
 // Create function for detecting game win state
+// Create function for resetting the game
+
 // Create function for movement for arrow buttons
+const moveLeft = document.getElementById('left')
+moveLeft.onclick = (event) => {
+    console.log('left has been pressed')
+}
+const moveRight = document.getElementById('right')
+moveRight.onclick = (event) => {
+    console.log('right has been pressed')
+}
+const moveUp = document.getElementById('up')
+moveUp.onclick = (event) => {
+    console.log('up has been pressed')
+}
+const moveDown = document.getElementById('down')
+moveDown.onclick = (event) => {
+    console.log('down has been pressed')
+}
 
+let enemyGhost = new Spooky("Resources\\spooky.png", 100, 100);
+// Create start button listener to execute start()
+start.addEventListener('click', gameStart);
 
+// Setting repeating game timer loop
 
+// Create start button function
+function gameStart () {
+    start.value = 'game started';
+    // start gameLoop()
+    
+    enemyGhost.Sprite.onload = window.requestAnimationFrame(gameLoop)
+
+}
+
+function gameLoop(){
+    // Clearing canvas
+    ctx.clearRect(0, 0, divWidth, divHeight);
+    // Drawing ghost at coordinates x and y, with defined size.
+    ctx.drawImage(enemyGhost.Sprite, enemyGhost.X, enemyGhost.Y, 80, 80);
+    // Start ghost movement and collision
+    enemyGhost.moveBounce();
+    // Declaring time frame for interval checking. 
+    window.requestAnimationFrame(gameLoop);
+}
