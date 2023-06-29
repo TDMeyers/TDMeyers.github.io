ead6547f2565d4145d60ae0a9682b1c73356b4c9
TDMeyers <meyers.dylan@outlook.com>
2023-06-29 15:08:55 -0600

    Readme, fail+win states, formatting tweak

--- README.md
+++ README.md
@@ -1,8 +1,151 @@
 # TDMeyers.github.io
-Hosted on Github Pages at https://tdmeyers.github.io/
-DOM based browser game - Project 1 for Activate Work and Per Scholas
 
+Cave Explorer DOM based browser game 
+------------------------------------------------------------------
+(not the most interesting title, but it gets the point across!)
+- Project 1 for Activate Work and Per Scholas
+------------------------------------------------------------------
+Hosted on Github Pages at https://tdmeyers.github.io/
+------------------------------------------------------------------
+
+----------------- Motivations ------------------------------------
+I wanted to make a game that would be a stretch of my capabilities as a burgeoning learner and software developer. Having had no prior exposure to code of any sort, I felt this was an excellent benchmarking opportunity to conclude the first Module of the Activate Work / Per Scholas journey. I can confidently say that before beginning the program I would absolutely NOT have been able to produce anything like this - amateur hour it may be - no matter how many hours went in. As it stands, I plan to continue to invest time into polish and development as I believe it is an excellent display of a variety of web developement skills and techniques. This will likely become one of my highlighted portfolio projects, and I'm excstatic that I was able to reach even this point (per my commit today on 6/29/2023). Can't wait to see where I'll be able to take it. Thank you to all the amazing artists and graphics designers that allow their work to be shared under the various creative commons licenses - the world would be far duller without your shine. 
+
+--------------------------- Build Status -------------------------
+Currently still very much a W.I.P and only JUST BARELY meets the MVP guidelines of the assignment. I still plan on continuing to add objects such as visible walls and rocks. Also on the docket is improving the overal UI/UX to something more cohesive and staging more animations and text. I'l list out some extra ideas below as they come to me. 
+
+- Lives 
+- Extra levels
+- Timed/limited battery
+- Stamina tracker
+
+---------------------------- Code Style --------------------------
+Honestly, as far as I'm aware the code style is pretty standard. I've tried to use Semantic HTML when possible, and keep the logic of the game separated out without too much codependency or overlap. I have also attempted to be verbose in my comments in the JS page, so that the overall layout can be followed. 
+
+--------------------------- Screenshots? ------------------------
+
+Find one linked here, maybe: 
+
+
+--------------------------- Tech / Framework --------------------
+
+In the spirit of the project I have tried to utilize outside libraries as little as possible and keep the scope constrained to HTML/CSS/JavaScript. I did utilize Canvas and its myriad of capabilites for the game window and plan on extending the animation framework in the future for more visually engaging gameplay. 
+
+
+--------------------------- Features -----------------------------
+
+One point that I really tried to focus on in this project is making it display-dynamic and responsive. I have tried to hardcode as few values as possible and thus the overall look and feel should change only slightly, within reason, between various sizes of displays.  
+
+-------------------------- Code Ex: -------------------------------
+This is a snapshot of the loop that begins the "game run", which draws the objects on screen and begins movement and checking for various collisions and interactions. 
+
+-------------------------------------------------------------------
+
+// Create start button function
+function gameStart () {
+    start.value = 'game started';
+    // start gameLoop()
+    // Load in Player and Ghost characters
+    enemyGhost.Sprite.onload = window.requestAnimationFrame(gameLoop);
+    newPlayer.Sprite.onload = window.requestAnimationFrame(gameLoop);
+    firstGate.Sprite.onload = window.requestAnimationFrame(gameLoop);
+
+}
+
+function gameLoop(){
+    // Clearing canvas
+    ctx.clearRect(0, 0, canvasW, canvasH);
+    // Drawing ghost at coordinates x and y, with defined size.
+    ctx.drawImage(enemyGhost.Sprite, enemyGhost.X, enemyGhost.Y, 80, 80);
+    // Drawing player
+    ctx.drawImage(newPlayer.Sprite, newPlayer.X, newPlayer.Y, 80, 80);
+
+    ctx.drawImage(firstGate.Sprite, firstGate.X, firstGate.Y, 25, 100)
+    // Start ghost movement and wall? collision
+    enemyGhost.moveBounce();
+    // start player wall? collision
+    newPlayer.collision();
+    // start player and ghost collisions
+    firstGate.gateColision();
+
+    checkCollide();
+
+    // Declaring time frame for interval checking. 
+    window.requestAnimationFrame(gameLoop);
+
+By looping through and using the RequestAnimationFrame, I can time the redrawing of the canvas to the refresh rate of the browser and display and attempt to ensure that all objects are kept onscreen at roughly the same rate and method. This also keeps the function checks consistent at the same pace so that one check does not happen significantly faster or slower than the others. 
+
+--------------------------------------------------------------------
+-------------------------- Installation ---------------------------
+This will be to host from your own GitHub pages.io site, cloned from this project! The initial configuration steps can also be found on https://pages.github.com/ 
+
+- Make a new github repo for your project, naming it username.github.io, remmeber to keep it public so that it can be deployed. 
+
+From Git Bash:
+
+cd into your desired directory 
+
+type "git clone" and then paste the following:
+
+git clone https://github.com/username/username.github.io
+
+- enter your project folder and add an index.html file, like so:
+cd username.github.io
+touch index.html
+
+then push it! (push it~)
+
+git add --all
+git commit -m "initial commit"
+git push -u origin main
+
+then cd back to your 'main' directory
+
+type "git clone" and paste the following:
+
+https://github.com/TDMeyers/TDMeyers.github.io
+
+press "enter" to create your local clone. 
+
+---------------------- How to Play: --------------------------------
+
+Pretty simple! Start with the labeled button, move with the provided arrow keys. Depending on when you see this, the flashlight may-or-may-not be working. But try the buttons out anyway!
+
+
+---------------------- Want to Contribute? -------------------------
+
+You're awesome for doing so!! Please keep in mind that I am at the very basic levels of publishing this so some things may look clumsy and/or not be optimized. This is intentional and OK, as this is a project for learning - not monetization. 
+
+Feel free to propose changes or constructive criticism in a pull request. 
+
+Issue Focuses:
+
+- What browser are you using? Does it occur on all browsers or just one? 
+
+- What resolution does the error occur on? Is it repeatable on multiple screen sizes? 
+
+- Screenshots are very helpful, but I understand the current limitations.
+
+- Expected and/or deviated behavior. 
+
+- If you're so inclined - browser version. 
+
+The more detail, the less chances of a miscommunication. Thank you for assisting me in the journey of learning!
+
+
+--------------------- Licensing -----------------------------------
+
+This is created under the GNU Affero General Public License v3.0
+
+Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.
+
+--------------------------------------------------------------------
 
 Flashlight image from https://pngimg.com/uploads/flashlight/flashlight_PNG11108.png
 
 Coin slot image from https://www.publicdomainpictures.net/en/view-image.php?image=285805&picture=drop-coin-slot
+
+Awesome pixel art explorer credit to Chr0ma Dave at 
+https://chroma-dave.itch.io/pixelart-hiker
+
+Portal art from: https://www.pngall.com/portal-png/download/33537
--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 // Create global variables for drawing the game window
-const
-divHeight = document.getElementById('game-portal').clientHeight;
-divWidth = document.getElementById('game-portal').clientWidth;
+const divHeight = document.getElementById('game-portal').clientHeight;
+const divWidth = document.getElementById('game-portal').clientWidth;
 
 // Create global variables for drawing maps onto canvas
-const 
-canvas = document.getElementById("game-view");
-ctx = canvas.getContext("2d");
+const canvas = document.getElementById("game-view");
+const ctx = canvas.getContext("2d");
 let offsetX = canvas.offsetLeft;
 let offsetY = canvas.offsetTop;
+console.log(ctx)
+
+//Adjust Canvas dimensions to containing div
+canvas.width = divWidth
+canvas.height = divHeight
+
+// Confirm correct canvas width and height dimensions adjusted to parent containing div
+const canvasW = canvas.getBoundingClientRect().width;
+const canvasH = canvas.getBoundingClientRect().height;
+console.log(canvasW, canvasH)
 
 // Create global variables for button functions
-const
-// - Directionals
-left = document.getElementById("left");
-right = document.getElementById("right");
-up = document.getElementById("up");
-down = document.getElementById("down");
+// Directionals 
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const up = document.getElementById("up");
+const down = document.getElementById("down");
 // - Flashlight
-turnOn = document.getElementById("on");
-turnOff = document.getElementById("off");
+const turnOn = document.getElementById("on");
+const turnOff = document.getElementById("off");
 // - Game Controls
-start = document.getElementById("start");
-reset = document.getElementById("reset");
-reset.addEventListener('click', function(event){
-    location.reload();
+const start = document.getElementById("start");
+const gameReset = document.getElementById("reset");
+gameReset.addEventListener('click', function(event){
+    document.location.reload();
+    clearInterval(interval);
 })
 // Creating function to draw Canvas element onto page. 
 function createCanvas() {
-    canvas.height = divHeight;
-    canvas.width = divWidth;
+    canvas.height = canvasH;
+    canvas.width = canvasW;
     }
 // Drawing Canvas area to place game window in on document load.
 window.addEventListener('load', function(event){
@@ -39,18 +47,18 @@ window.addEventListener('load', function(event){
 // Adding resize listener so that the game window adjusts automatically to resize events.
 window.addEventListener('resize', function(event){
     createCanvas();
-    }, true )
+})
 
 // Create class for Player character
 class Player{
     constructor(img, x, y, dx, dy, radius){
-        this.Sprite = new Image();
+        this.Sprite = new Image(80, 80);
         this.Sprite.src = img;
         this.X = x;
         this.Y = y;
-        this.dx = dx || 20;
-        this.dy = dy || 20;
-        this.radius = radius || 80;
+        this.dx = dx || (canvasW / 15);
+        this.dy = dy || (canvasH / 10);
+        // this.radius = radius || 80;
     }
         // Movement functions for Player
         moveUp(){
@@ -65,27 +73,64 @@ class Player{
         moveRight(){
             this.X += this.dx;
         }
-        // Creating collision function for Player 
+        // Creating collision function for Player + Boundaries
         collision() {
-        if (this.X + this.dx > divWidth-this.radius || this.X + this.dx < 0) {
-            this.dx = -this.dx;
+            // Checking boundaries for X axis right side
+        if (this.X + (this.dx / 4) >= (canvasW-this.Sprite.width)){
+            // preventing further right movement
+            this.X --;
+            // Checking boundaries for X axis left side 
+        } else if (this.X + this.dx <= (0+ this.Sprite.width)){
+            // preventing further left movement
+            this.X ++;
+            
         }
-        
-        if (this.Y + this.dy > divHeight-this.radius || this.Y + this.dy < 0) {
-            this.dy = -this.dy;
-        }
+            // Checking boundaries for Y axis top side
+        if (this.Y + (this.dy / 2) > (canvasH-this.Sprite.height)) {
+            // preventing further downwards movement
+            this.Y --;
+            // Checking boundaries for Y axis top side
+        } else if (this.Y + this.dy <= (0 + this.Sprite.height)){
+            // preventing further upwards movement
+            this.Y ++;
+        } 
     }
 }
+class Rocks{
+    constructor(img, x, y){
+        this.Sprite = new Image(50, 50);
+        this.Sprite.src = img;
+        this.X = x;
+        this.Y = y;
+    }
+}
+class Gateway{
+    constructor(img, x, y){
+        this.Sprite = new Image(25, 100);
+        this.Sprite.src = img;
+        this.X = x;
+        this.Y = y;
+    }
+    gateColision(){
+        if (newPlayer.X + newPlayer.dx >= this.X && newPlayer.Y + newPlayer.dx >= this.Y) {
+            window.alert(`Into the unknown! (You've lived another day)`);
+            document.location.reload();
+            clearInterval(interval);
+        }
+    } 
+}
+
+    
 // class for randomly moving Spooky object
 class Spooky{
     constructor(img, x, y, dx, dy, radius){
-        this.Sprite = new Image();
+        this.Sprite = new Image(80, 80);
         this.Sprite.src = img;
         this.X = x;
         this.Y = y;
         this.dx = dx || 1;
         this.dy = dy || 1;
-        this.radius = radius || 80;
+        // this.radius = radius || 80;
     }
 
     moveBounce() {
@@ -94,11 +139,13 @@ class Spooky{
         this.Y += this.dy;
         
         // Creating collision function for Spooky 
-        if (this.X + this.dx > divWidth-this.radius || this.X + this.dx < 0) {
+        // if (this.X + this.dx > divWidth-this.radius || this.X + this.dx < 0) {
+            if (this.X + this.dx > canvasW-this.Sprite.width || this.X + this.dx < 0) {
+
             this.dx = -this.dx;
         }
         
-        if (this.Y + this.dy > divHeight-this.radius || this.Y + this.dy < 0) {
+        if (this.Y + this.dy > canvasH-this.Sprite.height || this.Y + this.dy < 0) {
             this.dy = -this.dy;
         }
     }
@@ -111,6 +158,7 @@ class Spooky{
 // Create function for detecting game win state
 // Create function for resetting the game
 let newPlayer = new Player("Resources\\cover.png", 100, 100)
+let firstGate = new Gateway("Resources\\gateway.png", (canvasW - 25), (canvasH - 100))
 
 // Create function for movement for arrow buttons
 const moveLeft = document.getElementById('left')
@@ -134,15 +182,17 @@ moveDown.onclick = (event) => {
     console.log('down has been pressed')
 }
 
-let enemyGhost = new Spooky("Resources\\spooky.png", 1000, 500);
+let enemyGhost = new Spooky("Resources\\spooky.png", (canvasW * 0.8), (canvasH * 0.8));
 
 // Create function for collisions
 function checkCollide() {
-    if (newPlayer.X < enemyGhost.X + (enemyGhost.radius - 15) &&
-        newPlayer.X + (newPlayer.radius - 15) > enemyGhost.X &&
-        newPlayer.Y < enemyGhost.Y + (enemyGhost.radius - 15) &&
-        (newPlayer.radius - 15) + newPlayer.Y > enemyGhost.Y) {
-            window.alert('Game over!')
+    if (newPlayer.X < enemyGhost.X + (enemyGhost.Sprite.width / 1.7) &&
+        newPlayer.X + (newPlayer.Sprite.width / 1.7) > enemyGhost.X &&
+        newPlayer.Y < enemyGhost.Y + (enemyGhost.Sprite.height / 1.7) &&
+        (newPlayer.Sprite.height / 1.7) + newPlayer.Y > enemyGhost.Y) {
+            window.alert('Game over!');
+            document.location.reload();
+            clearInterval(interval);
         }
 }
 
@@ -159,22 +209,29 @@ function gameStart () {
     // Load in Player and Ghost characters
     enemyGhost.Sprite.onload = window.requestAnimationFrame(gameLoop);
     newPlayer.Sprite.onload = window.requestAnimationFrame(gameLoop);
+    firstGate.Sprite.onload = window.requestAnimationFrame(gameLoop);
 
 }
 
 function gameLoop(){
     // Clearing canvas
-    ctx.clearRect(0, 0, divWidth, divHeight);
+    ctx.clearRect(0, 0, canvasW, canvasH);
     // Drawing ghost at coordinates x and y, with defined size.
     ctx.drawImage(enemyGhost.Sprite, enemyGhost.X, enemyGhost.Y, 80, 80);
     // Drawing player
-    ctx.drawImage(newPlayer.Sprite, newPlayer.X, newPlayer.Y, 80, 80)
+    ctx.drawImage(newPlayer.Sprite, newPlayer.X, newPlayer.Y, 80, 80);
+
+    ctx.drawImage(firstGate.Sprite, firstGate.X, firstGate.Y, 25, 100)
     // Start ghost movement and wall? collision
     enemyGhost.moveBounce();
     // start player wall? collision
     newPlayer.collision();
     // start player and ghost collisions
+    firstGate.gateColision();
+
     checkCollide();
+
     // Declaring time frame for interval checking. 
     window.requestAnimationFrame(gameLoop);
 }
+
